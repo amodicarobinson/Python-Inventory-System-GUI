2da2309e6bd5f7d99200198a749e55a6fc18e151
amodicarobinson <amodicarobinson@gmail.com>
2022-08-18 11:46:57 -0600

    Tested functionality of the App

--- Tkinter Practice.py
+++ Tkinter Practice.py
@@ -11,7 +11,7 @@ from csv import *
 win = tk.Tk()
 win.title("Python Inventory GUI")
 win.geometry("500x500")
-win.config(background="black")
+win.config(background="white")
 main_lst=[]
 # Exit Action
 def _quit():
